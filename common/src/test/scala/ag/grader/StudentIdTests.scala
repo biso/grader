adfbf2352c626e5d668290dcd3f8294052258335
Ahmed Gheith <ag@gheith.com>
2022-07-23 16:12:19 -0500

    remove any_extension

--- common/src/test/scala/ag/grader/StudentIdTests.scala
+++ common/src/test/scala/ag/grader/StudentIdTests.scala
@@ -5,8 +5,6 @@ import upickle.default.write
 class StudentIdTests extends munit.FunSuite {
   test("t1") {
     val s = Name[Student]("ag")
-    write(s) |> println
-    write("ag") |> println
     assert(clue(write(s)) == clue("\"ag\""))
   }
 }
